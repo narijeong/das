2bbfd72d73523f5373d0fb4da76adaace87adea3
Nari Jeong <narijeong@icloud.com>
2022-06-10 07:26:07 +0900

    fix typo in  graphy.py

--- data-structure/graph.py
+++ data-structure/graph.py
@@ -31,15 +31,31 @@ class Graph:
 
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            self.adj_list[v1].remove(v2)
-            self.adj_list[v2].remove(v1)
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError: # in case there is no edge between v1 and v2
+                pass
             return True
         return False
 
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+            
+
+
     
-# graph = Graph()
-# graph.add_vertex('A')
-# graph.add_vertex('B')
-# graph.add_edge('A', 'B')
-# graph.remove_edge('A', 'B')
-# graph.print_graph()
+graph = Graph()
+graph.add_vertex('A')
+graph.add_vertex('B')
+graph.add_vertex('C')
+graph.add_edge('A', 'B')
+graph.add_edge('A', 'C')
+graph.remove_edge('A', 'B')
+graph.remove_vertex('A')
+graph.print_graph()
