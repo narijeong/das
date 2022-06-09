022bb5f4ebf379d48d8e2ad093ad9794f570abd3
Nari Jeong <narijeong@icloud.com>
2022-06-10 04:28:09 +0900

    add queue.py and stack.py

---
+++ data-structure/stack.py
@@ -0,0 +1,36 @@
+# LIFO
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    # push a new to top
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+            
+    # remove and return the top Node
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        return temp.value
