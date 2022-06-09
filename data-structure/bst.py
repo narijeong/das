1c21f362e5c5bd3b65941d5a5f7b7a5620ad2ffa
Nari Jeong <narijeong@icloud.com>
2022-06-10 03:13:40 +0900

    add bst.py

---
+++ data-structure/bst.py
@@ -0,0 +1,80 @@
+# tree can be build with with a form of LinkedList
+
+# Terminologies
+# Full tree: every node point to 0 or 2 nodes
+# perfect tree: in every level in the tree is filled
+# complete tree: filling the tree from left to right without gap
+
+
+# build a binary tree
+# bst = divide and conquere
+# number of nodes = 2^n - 1 where n is level
+# lookup, insert, remove: O(log n)
+# technically the worst case is O(n); n is # of nodes 
+# when adding 80 to a bst: (10)-(20)-(30)-(40)-(insert)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def print_bst(self):
+        temp = self.root
+        while temp is not None:
+            print(temp.left.value)
+            temp = temp.left
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if value == temp.value:
+                return False
+            if value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        # not neccessary
+        # if self.root is None:
+        #     return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+    def minimum_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node
+
+# test 
+# bst = BinarySearchTree()
+# bst.insert(47)
+# bst.insert(21)
+# bst.insert(76)
+# print(bst.root.value)
+# print(bst.root.left.value)
+# print(bst.root.right.value)
+# print(bst.contains(21))
+# print(bst.contains(20))
+# print(bst.minimum_value(bst.root.left))
