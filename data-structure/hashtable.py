e39d3d805a22f120e19c76948604f6ea08268e0f
Nari Jeong <narijeong@Nariui-MacBookPro.local>
2022-06-09 17:41:19 +0900

    add hashtable.py

---
+++ data-structure/hashtable.py
@@ -0,0 +1,49 @@
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None]*size
+    
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+    # for collision use 'seperate chining'
+    # but this deosn't take care of the case where same key is added
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    # return value for key
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
+    # return the list of keyes
+    def keys(self):
+        all_keys =[]
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+
+ht = HashTable()
+ht.set_item('bolts', 1400)
+ht.set_item('washers', 50)
+ht.set_item('bolts', 20000)
+print(ht.get_item('bolts'))
+print(ht.get_item('lumber'))
+ht.print_table()
+print(ht.keys())
