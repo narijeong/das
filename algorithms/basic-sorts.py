7d8d5c3902b94acbc4e863b30dca5c074bac5817
Nari Jeong <narijeong@icloud.com>
2022-06-13 15:10:35 +0900

    change filename basic-sorts.py

--- algorithms/basic-sorts.py
+++ algorithms/basic-sorts.py
@@ -44,34 +44,3 @@ def insertion_sort(my_list):
 
 print('insertion sort')
 print(insertion_sort([4,2,6,5,1,3]))        
-
-# helper function
-def merge(list1, list2):
-    combined = []
-    i, j = 0, 0
-    while i < len(list1) and j  < len(list2):
-        if list1[i] < list2[j]:
-            combined.appnend(list1[i])
-            i += 1
-        else:
-            combined.append(list2[j])
-            j += 1
-
-    while i < len(list1):
-        combined.append(list1[i])
-        i += 1
-    while j < len(list2):
-        combined.append(list2[j])
-        j += 1
-    return combined
-    
-# break list in half
-# base case when len(the_list) == 1
-# use merge() to put list together
-def merge_sort(my_list):
-    if len(my_list) == 1:
-        return my_list
-    mid = len(my_list)//2
-    left = my_list[:mid]
-    right = my_list[mid:]
-    return merge(merge_sort(left), merge_sort(right))
